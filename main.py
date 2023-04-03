a260f0043f398988051ae46dd52bbc4747327699
MorningSnow <lunarari0301@gmail.com>
2023-04-04 00:16:41 +0900

    add some new genes

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from Bio import Entrez, SeqIO
 
-# NCBI 데이터베이스에 접속하기 위한 이메일 주소를 입력해주세요.
-Entrez.email = "lunarari0301@gmail.com"
+def get_sars_cov_2_sequence():
+    Entrez.email = "lunarari0301@gmail.com"
+    handle = Entrez.efetch(db="nucleotide", id="NC_045512.2", rettype="fasta")
+    record = SeqIO.read(handle, "fasta")
+    handle.close()
+    return str(record.seq)
 
-# SARS-CoV-2의 염기 서열을 다운로드합니다.
-handle = Entrez.efetch(db="nucleotide", id="NC_045512", rettype="fasta", retmode="text")
-record = SeqIO.read(handle, "fasta")
-sars_cov2_seq = str(record.seq)
+def calculate_similarity(seq1, seq2):
+    if len(seq1) != len(seq2):
+        raise ValueError("The lengths of two sequences are not equal.")
+    count = 0
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            count += 1
+    similarity = count / len(seq1)
+    return similarity
 
-# 대조할 염기 서열을 gene.txt로부터 가져옵니다.
-f = open("gene.txt", 'r')
-user_seq = f.readline()
+#user_sequence = input("Enter the DNA sequence: ")
+sars_cov_2_sequence = get_sars_cov_2_sequence()
+'''
+f = open('gene2.txt', 'w')
+f.write(sars_cov_2_sequence)
+f.close()
+'''
+f = open('gene2.txt', 'r')
+user_sequence = f.read()
 f.close()
 
-# 두 염기 서열을 비교합니다.
-if sars_cov2_seq == user_seq:
-    print("입력한 염기 서열과 SARS-CoV-2의 염기 서열이 일치합니다.")
-else:
-    print("입력한 염기 서열과 SARS-CoV-2의 염기 서열이 일치하지 않습니다.")
+similarity = calculate_similarity(user_sequence, sars_cov_2_sequence)
+
+print(f"The similarity between the user sequence and SARS-CoV-2 sequence is:{similarity*100 :.2f}%", )
